6c2484dfdd0b91236ca223de54201b47cc00d98e
Ben Taylor <benataylor1992@hotmail.co.uk>
2019-11-01 11:15:27 +0000

    Add files via upload

---
+++ Kata/Kata/Kata (6)/(6)Multiplesof3or5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata
+{
+    public class Multiplesof3or5
+    {
+        public static int Solution(int value)
+        {
+            var hello = 0;
+
+            for (int i = 3; i < value; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    hello += i;
+                }
+            }
+            return hello;
+
+        }
+    }
+}
---
+++ Kata/Kata/Kata (6)/(6)SumofDigits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kata
+{
+    public class SumofDigits
+    {
+        public int DigitalRoot(long n)
+        {
+            var hello = n.ToString().Select(x => int.Parse(x.ToString()));
+
+            var end = hello.Sum();
+
+            if (end > 10)
+            {
+                var start = end.ToString().Select(x => int.Parse(x.ToString()));
+                return start.Sum();
+            }
+
+            return end;
+        }
+    }
+}
