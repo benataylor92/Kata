959a59994ddefdd1ab36a847085e48d75e3c1e21
Ben Taylor <TAYLORBX@legal.regn.net>
2019-11-05 09:24:06 +0000

    updated Stop spinning my words

---
+++ Kata/Kata (6)/(6)StopSpinningMyWords.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kata.Kata__6_
+{
+    public class StopSpinningMyWords
+    {
+        public static string SpinWords(string sentence)
+        {
+            return String.Join(" ", sentence.Split(' ').Select(str => str.Length >= 5 ? new string(str.Reverse().ToArray()) : str));
+        }
+    }
+}
---
+++ KataTests/StopSpinningMyWordsTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class StopSpinningMyWordsTests
+    {
+        [Test]
+        public static void Test1()
+        {
+            Assert.AreEqual("emocleW", Kata.Kata__6_.StopSpinningMyWords.SpinWords("Welcome"));
+        }
+
+        [Test]
+        public static void Test2()
+        {
+            Assert.AreEqual("Hey wollef sroirraw", Kata.Kata__6_.StopSpinningMyWords.SpinWords("Hey fellow warriors"));
+        }
+
+        [Test]
+        public static void Test3()
+        {
+            Assert.AreEqual("This is a test", Kata.Kata__6_.StopSpinningMyWords.SpinWords("This is a test"));
+        }
+
+        [Test]
+        public static void Test4()
+        {
+            Assert.AreEqual("This is rehtona test", Kata.Kata__6_.StopSpinningMyWords.SpinWords("This is another test"));
+        }
+
+        [Test]
+        public static void Test5()
+        {
+            Assert.AreEqual("You are tsomla to the last test", Kata.Kata__6_.StopSpinningMyWords.SpinWords("You are almost to the last test"));
+        }
+
+        [Test]
+        public static void Test6()
+        {
+            Assert.AreEqual("Just gniddik ereht is llits one more", Kata.Kata__6_.StopSpinningMyWords.SpinWords("Just kidding there is still one more"));
+        }
+    }
+}
