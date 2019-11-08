b2a832ad3e9a97aff4ee0445eb622c80ffa7bbf6
Ben Taylor <TAYLORBX@legal.regn.net>
2019-11-08 12:23:30 +0000

    updated IQ Test

---
+++ Kata/Kata (6)/(6)IQTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kata.Kata__6_
+{
+    public class IQTest
+    {
+        public static int Test(string numbers)
+        {
+            int[] Timmy = numbers.Split(' ').Select(int.Parse).ToArray();
+
+            int firstThreeParity = Timmy[0] % 2 + Timmy[1] % 2 + Timmy[2] % 2;
+
+            int rem = firstThreeParity < 2 ? 1 : 0;
+            var value = 0;
+
+            for (int i = 1; i < Timmy.Length; i++)
+            {
+                if(Timmy[i] % 2 == rem)
+                {
+                    value = i;
+                }
+            }
+            return value + 1;
+        }
+    }
+}
---
+++ KataTests/IQTestTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class IQTestTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(3, Kata.Kata__6_.IQTest.Test("2 4 7 8 10"));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(1, Kata.Kata__6_.IQTest.Test("1 2 2"));
+        }
+    }
+}
