dbafdf25f1fdfec4e5db70aa67912cd29c4a2cfc
Ben Taylor <TAYLORBX@legal.regn.net>
2019-11-08 10:26:31 +0000

    updated new Kata

---
+++ Kata/Kata (6)/(6)PersistentBugger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kata.Kata__6_
+{
+    public class PersistentBugger
+    {
+        public static int Persistence(long n)
+        {
+            int count = 0;
+            while (n > 9)
+            {
+                count++;
+                n = n.ToString().Select(digit => int.Parse(digit.ToString())).Aggregate((x, y) => x * y);
+            }
+            return count;
+        }
+    }
+}
---
+++ KataTests/PersistentBuggerTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class PersistentBuggerTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine("****** Basic Tests");
+            Assert.AreEqual(3, Kata.Kata__6_.PersistentBugger.Persistence(39));
+            Assert.AreEqual(0, Kata.Kata__6_.PersistentBugger.Persistence(4));
+            Assert.AreEqual(2, Kata.Kata__6_.PersistentBugger.Persistence(25));
+            Assert.AreEqual(4, Kata.Kata__6_.PersistentBugger.Persistence(999));
+        }
+    }
+}
