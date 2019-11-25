2f893d6519acd6fbbc2a83f61d958487a736b3f2
Ben Taylor <TAYLORBX@legal.regn.net>
2019-11-25 16:59:41 +0000

    Added the Simple Pig Latin functionality

---
+++ Kata/kata (5)/(5)SimplePigLatin.cs
@@ -0,0 +1,53 @@
+﻿using EO.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kata.kata__5_
+{
+    public class SimplePigLatin
+    {
+        public static string PigIt(string str)
+        {
+            //string[] arrOfString = str.Split(' ');
+            //string start = " ";
+            //foreach (string a in arrOfString)
+            //{
+            //    start += a.Substring(0, a.Length - 1) + " ";
+            //    start += "ay";
+            //}
+            //return start;
+
+            string[] arrayOfString = str.Split(' ');
+
+            List<string> finish = new List<string>();
+
+            foreach (string word in arrayOfString)
+            {
+                var result = word.Skip(1).Concat(word.Take(1));
+
+                string start = string.Join("", result);
+
+                start += @"ay";
+
+                finish.Add(start);
+            }
+
+            var expected = string.Join(" ", finish);
+
+
+            return expected;
+
+            //string[] arrayOfString = str.Split(' ');
+
+            //char[] firstword = arrayOfString[0].ToCharArray();
+
+            //var result = firstword.Skip(1).Concat(firstword.Take(1));
+
+            //string s1 = string.Join("", result);
+
+            //s1 += @"ay";
+        }
+    }
+}
---
+++ KataTests/SimplePigLatinTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class SimplePigLatinTests
+    {
+        [Test]
+        public void KataTests()
+        {
+            Assert.AreEqual("igPay atinlay siay oolcay", Kata.kata__5_.SimplePigLatin.PigIt("Pig latin is cool"));
+            Assert.AreEqual("hisTay siay ymay tringsay", Kata.kata__5_.SimplePigLatin.PigIt("This is my string"));
+        }
+    }
+}
