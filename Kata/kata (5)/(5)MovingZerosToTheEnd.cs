bae9d151ebf895ebfae4887ce0c8e58af6aa5bd9
Ben Taylor <TAYLORBX@legal.regn.net>
2019-11-26 11:20:14 +0000

    completed the Moving Zeros to the end test

---
+++ Kata/kata (5)/(5)MovingZerosToTheEnd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata.kata__5_
+{
+    public class MovingZerosToTheEnd
+    {
+        public static int[] MoveZeroes(int[] arr)
+        {
+            int i = 0;
+
+            for (int j = 0; j < arr.Length; j++)
+                if (arr[j] != 0)
+                    arr[i++] = arr[j];
+
+            while (i < arr.Length)
+                arr[i++] = 0;
+
+            return arr;
+        }
+    }
+}
+
---
+++ KataTests/MovingZerosToTheEndTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class MovingZerosToTheEndTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(new int[] { 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 }, Kata.kata__5_.MovingZerosToTheEnd.MoveZeroes(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 }));
+        }
+    }
+}
