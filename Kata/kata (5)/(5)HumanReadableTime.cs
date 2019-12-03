217d46f034807a7075c74bf23ddf321027a634bc
Ben Taylor <TAYLORBX@legal.regn.net>
2019-12-03 16:50:10 +0000

    fixed the human readable time method

---
+++ Kata/kata (5)/(5)HumanReadableTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata.kata__5_
+{
+    public class HumanReadableTime
+    {
+        public static string GetReadableTime(int seconds)
+        {
+            var result = TimeSpan.FromSeconds(seconds);
+
+            var time1 = Math.Floor(result.TotalHours);
+
+            string time = $"{time1:00}:{result.Minutes:00}:{result.Seconds:00}";
+
+            return time;
+        }
+    }
+}
---
+++ KataTests/HumanReadableTimeTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class HumanReadableTimeTest
+    {
+        [Test]
+        public void HumanReadableTest()
+        {
+            Assert.AreEqual("00:00:00", Kata.kata__5_.HumanReadableTime.GetReadableTime(0));
+            Assert.AreEqual("00:00:05", Kata.kata__5_.HumanReadableTime.GetReadableTime(5));
+            Assert.AreEqual("00:01:00", Kata.kata__5_.HumanReadableTime.GetReadableTime(60));
+            Assert.AreEqual("23:59:59", Kata.kata__5_.HumanReadableTime.GetReadableTime(86399));
+            Assert.AreEqual("99:59:59", Kata.kata__5_.HumanReadableTime.GetReadableTime(359999));
+        }
+    }
+}
