7f19a803cdb284be7188a5981e2a72e146bb00ae
Ben Taylor <TAYLORBX@legal.regn.net>
2019-12-06 16:54:33 +0000

    Completed the Where My Anagram At

---
+++ Kata/kata (5)/(5)WhereMyAnagramsAt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Kata.kata__5_
+{
+    public class WhereMyAnagramsAt
+    {
+        public static List<string> Anagrams(string word, List<string> words)
+        {
+            string word1 = String.Concat(word.OrderBy(c => c));
+
+            List<string> test = new List<string>();
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                string words2 = String.Concat(words[i].OrderBy(c => c));
+
+                if (word1 == words2)
+                {
+                    test.Add(words[i]);
+                }
+
+            }
+            return test;
+        }
+
+        //return words?.Where(w => w.OrderBy(c => c).SequenceEqual(word.OrderBy(c => c))).ToList();
+    }
+}
---
+++ KataTests/WhereMyAnagramsAtTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class WhereMyAnagramsAtTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(new List<string> { "a" }, Kata.kata__5_.WhereMyAnagramsAt.Anagrams("a", new List<string> { "a", "b", "c", "d" }));
+            Assert.AreEqual(new List<string> { "carer", "arcre", "carre" }, Kata.kata__5_.WhereMyAnagramsAt.Anagrams("racer", new List<string> { "carer", "arcre", "carre", "racrs", "racers", "arceer", "raccer", "carrer", "cerarr" }));
+        }
+    }
+}
