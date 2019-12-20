b44b5a70b9645f1c0cc41d090b0236b8a338c4ef
Ben Taylor <TAYLORBX@legal.regn.net>
2019-12-20 14:28:20 +0000

    Added the weight for weight tests

---
+++ Kata/kata (5)/(5)WeightForWeight.cs
@@ -0,0 +1,21 @@
+﻿using OfficeOpenXml.FormulaParsing.Excel.Functions.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kata.kata__5_
+{
+    public class WeightForWeight
+    {
+        public static string orderWeight(string strng)
+        {
+            {
+                return string.Join(" ", strng.Split(' ')
+                    .OrderBy(n => n.ToCharArray()
+                    .Select(c => (int)char.GetNumericValue(c)).Sum())
+                    .ThenBy(n => n));
+            }
+        }
+    }
+}
