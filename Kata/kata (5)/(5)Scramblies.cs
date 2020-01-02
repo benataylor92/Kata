a956581b0893775b01d213f0ba6522f61d52f865
Ben Taylor <TAYLORBX@legal.regn.net>
2020-01-02 14:02:59 +0000

    added scramblies method

---
+++ Kata/kata (5)/(5)Scramblies.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kata.kata__5_
+{
+    public class Scramblies
+    {
+        public static bool Scramble(string str1, string str2)
+        {
+            foreach(var c1 in str2)
+            {
+                int index = 0;
+                if((index = str1.IndexOf(c1)) > -1)
+                {
+                    str1 = str1.Remove(index, 1);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+    }
+}
---
+++ KataTests/ScrambliesTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class ScrambliesTests
+    {
+
+        private static void testing(bool actual, bool expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public static void test1()
+        {
+            testing(Kata.kata__5_.Scramblies.Scramble("rkqodlw", "world"), true);
+            testing(Kata.kata__5_.Scramblies.Scramble("cedewaraaossoqqyt", "codewars"), true);
+            testing(Kata.kata__5_.Scramblies.Scramble("katas", "steak"), false);
+            testing(Kata.kata__5_.Scramblies.Scramble("scriptjavx", "javascript"), false);
+            testing(Kata.kata__5_.Scramblies.Scramble("scriptingjava", "javascript"), true);
+            testing(Kata.kata__5_.Scramblies.Scramble("scriptsjava", "javascripts"), true);
+            testing(Kata.kata__5_.Scramblies.Scramble("javscripts", "javascript"), false);
+            testing(Kata.kata__5_.Scramblies.Scramble("aabbcamaomsccdd", "commas"), true);
+            testing(Kata.kata__5_.Scramblies.Scramble("commas", "commas"), true);
+            testing(Kata.kata__5_.Scramblies.Scramble("sammoc", "commas"), true);
+        }
+    }
+}
